6f71b89992d960c3c3ec5a2bf809844c12f9af82
Rense-VS <112085210+Rense-VS@users.noreply.github.com>
2022-09-06 01:28:30 +0300

    Update main.py

--- flask/main.py
+++ flask/main.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-app.config["SECRET_KEY"] = "SPGKRPOKGBOGKTOKBOHK"
+app.config["SECRET_KEY"] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///base.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
